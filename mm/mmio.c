7a463cc182d87b2a422b5ec887c313bbbc711fb0
Quasimodo Tao <1429113692@qq.com>
2020-11-30 12:46:47 +0800

    master

--- mm/mmio.c
+++ mm/mmio.c
@@ -1,6 +1,21 @@
-//Lava OS
-//PageFault
-//20-03-10-21-15
+/*
+	mm/mmio.c
+	Copyright (C) 2020  Quasimodo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 #include <lava.h>
 #include <stddef.h>
